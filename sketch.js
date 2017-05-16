8ea4db65d3051e9f4b73085fedd3ee2bdd0d1469
Leslie Watkins <lesliewatkins@lesliewatkins-macbookpro.roam.corp.google.com>
2017-05-16 12:53:30 -0700

    added skeleton code

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var floor_height = 100;
+var block_size = 50;
+
+var x0 = 200; // starting position of block
+
+var h = 1/60; // timestep
+var x, v;     // position and velocity
+
+function setup() {
+    createCanvas(400, 400);
+    
+    // Set initial position and value.
+    x = x0;
+    v = 0;
+    
+    // Draw with thick black lines.
+    stroke(0);
+    strokeWeight(2);
+}
+
+function draw() {
+    // Draw on a white background.
+    background(255);
+    
+    // Drag the block to a different location
+    // with the mouse.
+    if (mouseIsPressed) {
+        x += (mouseX - pmouseX);
+    }
+    
+    // This is where the physics happens!
+    update();
+    
+    // Draw the floor.
+    line(0, floor_height,
+        width, floor_height);
+    
+    // Draw the block.
+    rect(x, floor_height - block_size,
+        block_size, block_size);
+}
+
+function update() {
+    // TODO: Implement the governing equations
+    // that will make the block move as if it's
+    // attached to a spring!
+}
